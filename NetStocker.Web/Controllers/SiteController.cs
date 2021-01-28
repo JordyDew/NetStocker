620ebb8a380de8e626f3842a35725413fe3568eb
Jordy Deweer <contact@jordydeweer.net>
2021-01-28 14:49:51 +0100

    Add a first controller to the app

---
+++ NetStocker.Web/Controllers/SiteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetStocker.Web.Controllers
+{
+    public class SiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
